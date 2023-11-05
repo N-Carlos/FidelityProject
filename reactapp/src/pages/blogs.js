c26514f61c5794da8541fd288118fc40acae300a
Nico <126415815+kokothemage@users.noreply.github.com>
2023-11-04 23:58:25 -0500

    blogs

--- reactapp/src/pages/blogs.js
+++ reactapp/src/pages/blogs.js
@@ -3,43 +3,20 @@ import Stylediv from "./form.js";
 
 
 const Blogs = () => {
-<input type="number"></input>
-  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
-  <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
-
-
-  return <form>
+  return <form> 
   <label form="fname">Initial Investment:</label>
-  <input type="text" InitialInvestment="fname" name="fname"></input>
+  <input type="number" id="InitialInvestment" name="InitialInvestment"></input>
     <label form="fname">Annual Return Rate:</label>
-  <input type="text" AnnualRR="fname" name="fname"></input>
+  <input type="number" id="AnnualRR" name="AnnualRR"></input>
   <label form="fname">Annual Management Fee:</label>
-  <input type="text" AnnualM="fname" name="fname"></input>
+  <input type="number" id="AnnualMF" name="AnnualMF"></input>
   <label form="fname">Contribution:</label>
-  <input type="text" Contribution="fname" name="fname"></input>
+  <input type="number" id="Contribution" name="Contrinbution"></input>
   <label form="fname">End Amount:</label>
-  <input type="text" EndA="fname" name="fname"></input>
+  <input type="number" id="EndA" name="fname"></input>
   <button type="button">Calculate</button>
+  </form>
+}
 
-  new Chart("myChart", {
-  type: "doughnut",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: "My Hedge Fund"
-    }
-  }
-});
-
-
-</form>;
-};
 
 export default Blogs;
