05707d181c97ef2eab7a3a64bfe3080daf3b32be
Alish Sharma <75708780+alishsharma17@users.noreply.github.com>
2023-11-05 02:55:58 -0600

    Final Table with Data
    inputed the mock data for the hedge fund information

--- reactapp/src/pages/index.js
+++ reactapp/src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import { Button } from "../components/Navbar/NavbarElements"; 
 
 const Home = () => {
   return (
     <div>
       <h1>Reverse Hedge Funds</h1>
-        <p>
-          Hedge funds are actively managed alternative investments that commonly use risky investment strategies. 
-          Hedge fund investment requires a high minimum investment or net worth from accredited investors. 
-          Hedge funds charge higher fees than conventional investment funds.
-        </p>
-        
+      <p>
+        Hedge funds are actively managed alternative investments that commonly use risky investment strategies. 
+        Hedge fund investment requires a high minimum investment or net worth from accredited investors. 
+        Hedge funds charge higher fees than conventional investment funds.
+      </p>
+      <div className="navbar-container">
+        <Button img="profile_pic.png">Click Me</Button> 
+      </div>
     </div>
   );
 };
