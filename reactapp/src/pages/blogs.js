448b89e878aa111a02157b5f23f21dc07992338f
4n7hr4xx <146919819+4n7hr4xx@users.noreply.github.com>
2023-11-05 03:46:01 -0600

    calc
    calc

--- reactapp/src/pages/blogs.js
+++ reactapp/src/pages/blogs.js
@@ -1,20 +1,106 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Blogs = () => {
-  return <form> 
-  <label form="fname">Initial Investment:</label>
-  <input type="number" id="InitialInvestment" name="InitialInvestment"></input>
-    <label form="fname">Annual Return Rate:</label>
-  <input type="number" id="AnnualRR" name="AnnualRR"></input>
-  <label form="fname">Annual Management Fee:</label>
-  <input type="number" id="AnnualMF" name="AnnualMF"></input>
-  <label form="fname">Contribution:</label>
-  <input type="number" id="Contribution" name="Contrinbution"></input>
-  <label form="fname">End Amount:</label>
-  <input type="number" id="EndA" name="fname"></input>
-  <button type="button">Calculate</button>
-  </form>
-}
+
+  const formContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column', // Adjust this to 'row' if you want elements to display horizontally
+    gap: '20px', // Adjust the gap between elements
+    width: '80%',
+    margin: '0 auto'
+  };
+
+  const labelInputContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between', // Positions label and input to each end
+    alignItems: 'center', // Aligns elements vertically
+  };
+
+  const labelStyle = {
+    margin: '10px 0', // Add margin to create space between labels and inputs
+    // Add any other specific styles for labels if needed
+    minWidth: '100px'
+  };
+
+  const inputContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    flex: '1', // Input occupies remaining space
+    maxWidth: '300px', // Set a maximum width for inputs
+  };
+
+  const inputStyle= {
+    flex: '1',
+    maxWidth: '300px'
+  }
+
+  const resultContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // Define specific styles for the result container here
+  };
+  
+  const [result, setResult] = useState(null);
+
+
+
+  const calculateResult = () => {
+    const initialInvestment = parseFloat(document.getElementById('InitialInvestment').value) || 0;
+    const annualReturnRate = parseFloat(document.getElementById('AnnualRR').value) || 0;
+    const annualManagementFee = parseFloat(document.getElementById('AnnualMF').value) || 0;
+
+    const calculatedResult = initialInvestment * annualReturnRate + annualManagementFee;
+
+    setResult(calculatedResult);
+  };
+
+
+  return (
+    <div style={formContainerStyle}>
+      <form> 
+        <div style={labelInputContainerStyle}>
+          <label style={labelStyle} form="fname">Initial Investment:</label>
+          <div style={inputContainerStyle}>         
+            <input style={inputStyle} type="number" id="InitialInvestment" name="InitialInvestment"></input>
+          </div>
+        </div>
+
+
+        <div style={labelInputContainerStyle}>
+          <label style={labelStyle} form="fname">Annual Return Rate:</label>
+          <div style={labelStyle}>
+            <input style={inputStyle} type="number" id="AnnualRR" name="AnnualRR"></input>
+          </div>
+        </div>
+
+        <div style={labelInputContainerStyle}>
+          <label style={labelStyle} form="fname">Annual Management Fee:</label>
+          <div style={labelStyle}>
+            <input style={inputStyle} type="number" id="AnnualMF" name="AnnualMF"></input>
+          </div>
+        </div>
+
+        <div style={labelInputContainerStyle}>
+          <label style={labelStyle} form="fname">Contribution:</label>
+          <div style={labelStyle}>
+            <input style={inputStyle} type="number" id="Contribution" name="Contrinbution"></input>
+          </div>
+        </div>
+
+        <button style={{...labelStyle, padding:'5px 10px'}} type="button" onClick={calculateResult}>
+          Calculate</button>
+ 
+
+        <div style={resultContainerStyle}>
+          <label style={labelStyle}>Result:</label>
+          <div>{result}</div>
+          
+        </div>
+    </form>
+  </div>
+  );
+};
 
 
 export default Blogs;
--- reactapp/src/pages/contact.js
+++ reactapp/src/pages/contact.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Contact = () => {
+
+  const labelInputContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between', // Positions label and input to each end
+    alignItems: 'center', // Aligns elements vertically
+  };
+
   return (
     <div>
-      <h1>Mail us on feedback@geeksforgeeks.org</h1>
+      <h1 style={labelInputContainerStyle}>Mail us on feedback@geeksforgeeks.org</h1>
     </div>
   );
 };
